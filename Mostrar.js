78c5d7ef18999d3cae11419b625c1cb975b03ab2
GarciaMT <garcia091.mt@gmail.com>
2025-09-24 22:12:20 -0500

    Create Mostrar.js

---
+++ Mostrar.js
@@ -0,0 +1,11 @@
+router.get('/',(req, res)=>{
+    db.query('select * from usuario', //Mostrar todas la tabla 
+    (err,result) =>{
+        if(err)console.log(err) //Si algo sale mal 
+        else{
+            res.send(result)
+            console.log("Metodo Get", result) //Si todo funciona
+        }
+    }
+    )
+})
